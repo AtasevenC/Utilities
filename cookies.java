b574486df479137bd9c949a53437d865706c7270
Çağrı ATASEVEN <cataseven3443@gmail.com>
2020-06-20 15:21:33 +0200

    Add files via upload
    addSomething

--- cookies.java
+++ cookies.java
@@ -38,3 +38,35 @@ public class cookieRead extends BasePage{
     }
  }
 }
+
+
+//Use stored cookies to login information
+public static void main(String[] args){
+    try{
+    File file = new File("Cookies.data");
+    FileReader fileReader = new FileReader(file);
+    BufferedReader Buffreader = new BufferedReader(fileReader);
+    String strline;
+    while((strline=Buffreader.readLine())!=null){
+    StringTokenizer token = new StringTokenizer(strline,";");
+    String name = token.nextToken();
+    String value = token.nextToken();
+    String domain = token.nextToken();
+    String path = token.nextToken();
+    Date expiry = null;
+
+   String val;
+  if(!(val=token.nextToken()).equals("null")) {
+       expiry = new Date(val);
+   }
+   Boolean isSecure = new Boolean(token.nextToken()).booleanValue();
+   Cookie ck = new Cookie(name,value,domain,path,expiry,isSecure);
+   System.out.println(ck);
+   driver.manage().addCookie(ck); // This will add the stored cookie to current session
+   }
+ }
+}catch(Exception ex){
+     ex.printStackTrace();
+ }
+   driver.get("http://demo.guru99.com/test/cookie/selenium_aut.php");
+   
